73d68854ce039287670b9d2ac9ed95a803473406
Julio Reyes <juliojesusreyes@gmail.com>
2023-10-18 18:10:07 -0300

    Avanzando con logica.

--- JulioBot.js
+++ JulioBot.js
@@ -1,54 +1,53 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { REST } = require ('@discordjs/rest')
+const { Client, GatewayIntentBits, Partials } = require ('discord.js');
+const { Routes } = require ('discord-api-types/v9')
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 
-const { BOT_TOKEN } = require('./config.json');
+const fs = require('fs')
+const path = require('path')
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+let commandTmp = []
+let commands = []
 
-client.once (Events.ClientReady, c => {
+require('dotenv').config({
+    path: path.join(__dirname, '.env'),
+})
 
-	console.log (`Ready! Logged in as ${c.user.tag}`);
+const token = process.env.DISCORD_TOKEN
 
+client.once('ready', () => {
+    console.log('BultoBot Listo!')
 
-});
+    let commandsFiles = fs.readdirSync(path.join(__dirname, './commands'))
 
-client.commands = new Collection();
+    commandsFiles.forEach((file, i) => {
+        commandTmp[i] = require('./commands/' + file)
+        commands = [
+            ...commands,
+            {
+                name: file.split('.')[0],
+                description: commandTmp[i].description,
+                init: commandTmp[i].init,
+                options: commandTmp[i].options,
+            },
+        ]
+    })
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const rest = new REST({ version: '9' }).setToken(token)
+    rest.put(Routes.applicationCommands(client.application.id), {
+        body: commands,
+    })
+        .then(() => {
+            console.log('Comandos Cargados!')
+        })
+        .catch(console.error)
+})
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return
+    const { commandName } = interaction
+    const selectedCommand = commands.find(c => commandName === c.name)
+    selectedCommand.init(interaction, client)
+})
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-client.login (BOT_TOKEN);
+client.login (token)
